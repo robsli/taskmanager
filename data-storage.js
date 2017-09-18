51ae6a14e82fdd923e7954668f9cc3fc44c63c62
Robbie <robbieli121.zy@gmail.com>
2017-09-18 00:01:46 -0500

    New formatting style and archive functionality

--- data-storage.js
+++ data-storage.js
@@ -6,13 +6,14 @@ let dataStorage = function () {
         }
 
         function retrieveFromStorage () {
-            if (localStorage.getItem('taskList') === 'undefined') {
+            if (localStorage.getItem('taskList') === 'undefined' || localStorage.getItem('taskList') === null) {
                 return [];
             } else {
                 let parsedTaskList = JSON.parse(localStorage.getItem('taskList'));
                 for (task in parsedTaskList) {
                     parsedTaskList[task].createdDate = new Date(parsedTaskList[task].createdDate);
-                    parsedTaskList[task].dueDate = new Date(parsedTaskList[task].dueDate);                
+                    parsedTaskList[task].dueDate = new Date(parsedTaskList[task].dueDate);   
+                    parsedTaskList[task].lastUpdated = new Date(parsedTaskList[task].lastUpdated);             
                 }
                 return parsedTaskList;
             }
--- form-validation.js
+++ form-validation.js
@@ -1,36 +1,37 @@
 let formValidation = function () {
     
-       document.getElementById('task-name').addEventListener('focusout', validateTaskName);
-       document.getElementById('assigned-to').addEventListener('focusout', validateAssignedTo);
-       document.getElementById('due-date').addEventListener('focusout', validateDueDate);
+       document.getElementById('task-name').addEventListener('focusout', function () { validateTaskName(document.getElementById('task-name'), document.getElementById('task-name-error')); });
+       document.getElementById('assigned-to').addEventListener('focusout', function () { validateAssignedTo(document.getElementById('assigned-to'), document.getElementById('assigned-to-error')); });
+       document.getElementById('due-date').addEventListener('focusout', function () { validateDueDate(document.getElementById('due-date'), document.getElementById('due-date-error')); });
     
-       function validateTaskName () {
-           let taskNameError = document.getElementById('task-name-error');
-          
-           if (document.getElementById('task-name').value === "") {
-               taskNameError.innerHTML = "Task name cannot be left blank.";
+       document.getElementById('modal-task-name').addEventListener('focusout', function () { validateTaskName(document.getElementById('modal-task-name'), document.getElementById('modal-task-name-error')); });
+       document.getElementById('modal-assigned-to').addEventListener('focusout', function () { validateAssignedTo(document.getElementById('modal-assigned-to'), document.getElementById('modal-assigned-to-error')); });
+       document.getElementById('modal-due-date').addEventListener('focusout', function () {validateDueDate(document.getElementById('modal-due-date'), document.getElementById('modal-due-date-error')); });
+
+
+       function validateTaskName (taskName, taskNameError) {
+           if (taskName.value === "") {
+               taskNameError.innerHTML = "Please enter a task name.";
            } else {
                taskNameError.innerHTML = "";
                taskNameError.className = "";
            }
        }
     
-       function validateAssignedTo () {
-           let assignedToError = document.getElementById('assigned-to-error');
-    
-           if (document.getElementById('assigned-to').value === "") {
-               assignedToError.innerHTML = "Assigned to field cannot be left blank.";
+       function validateAssignedTo (assignedTo, assignedToError) {
+           if (assignedTo.value === "") {
+               assignedToError.innerHTML = "Please assign this task to someone.";
            } else {
                assignedToError.innerHTML = "";
                assignedToError.className = "";
            }
        }
     
-       function validateDueDate () {
-           let dueDateError = document.getElementById('due-date-error');
-    
-           if (document.getElementById('due-date').value === "") {
-               dueDateError.innerHTML = "Due date cannot be left blank.";
+       function validateDueDate (dueDate, dueDateError) {
+           if (dueDate.value === "") {
+               dueDateError.innerHTML = "Please enter a due date.";
+        //    } else if (Date(document.getElementById('due-date').value) < Date()) {
+        //        dueDateError.innerHTML = "Please enter a due date in the future.";
            } else {
                dueDateError.innerHTML = "";
                dueDateError.className = "";
@@ -39,13 +40,22 @@ let formValidation = function () {
     
        function validateCreateNewTask () {
           
-           if (document.getElementById('task-name').value === "") { validateTaskName(); }
-           if (document.getElementById('assigned-to').value === "") { validateAssignedTo(); }
-           if (document.getElementById('due-date').value === "") { validateDueDate(); }
+           validateTaskName(document.getElementById('task-name'), document.getElementById('task-name-error'));
+           validateAssignedTo(document.getElementById('assigned-to'), document.getElementById('assigned-to-error'));
+           validateDueDate(document.getElementById('due-date'), document.getElementById('due-date-error'));
     
-           if (document.getElementById('task-name').value === "" || document.getElementById('assigned-to').value === "" || document.getElementById('due-date').value === "") {
-               return false;
-           } else { return true;}
+           return (document.getElementById('task-name').value === "" || document.getElementById('assigned-to').value === "" || 
+                   document.getElementById('due-date').value === "") ? false : true;
+       }
+
+       function validateUpdateTask () {
+
+            validateTaskName(document.getElementById('modal-task-name'), document.getElementById('modal-task-name-error'));
+            validateAssignedTo(document.getElementById('modal-assigned-to'), document.getElementById('modal-assigned-to-error'));
+            validateDueDate(document.getElementById('modal-due-date'), document.getElementById('modal-due-date-error'));
+    
+            return (document.getElementById('modal-task-name').value === "" || document.getElementById('modal-assigned-to').value === "" || 
+                    document.getElementById('modal-due-date').value === "") ? false : true;
        }
     
        // Enter Enter key (13) pressed:
@@ -62,7 +72,8 @@ let formValidation = function () {
        });
     
        return {
-           validateCreateNewTask : validateCreateNewTask
+           validateCreateNewTask : validateCreateNewTask,
+           validateUpdateTask: validateUpdateTask
        }
     
    }();
